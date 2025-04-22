35a417fe135f876a60cfbc0037ee2a4793df38f0
Romeo Shamoun <170680705+7onyx7@users.noreply.github.com>
2025-04-21 19:48:04 -0500

    Remove dir

--- .next/static/chunks/src_app_page_tsx_8450df25._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/src_components_StarryBackground_tsx_05f715c7._.js",
-  "static/chunks/_51c95adb._.js"
-],
-    source: "dynamic"
-});
