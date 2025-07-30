bed87c5bcfa269f57aeedd9f663661eea57e04ea
Romeo Shamoun <170680705+7onyx7@users.noreply.github.com>
2025-07-30 00:30:41 -0500

    Update Notion site

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -45,9 +45,9 @@ const contactInfo = [
         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z"/>
       </svg>
     ),
-    title: 'Onyx Tech Solutions',
+    title: 'Coming soon',
     value: 'Check us out on Notion',
-    link: 'https://onyxtechsolutions.notion.site/index'
+    link: 'https://vibeaistealthstartup.notion.site/index'
   },
   {
     icon: (
